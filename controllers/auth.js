7b773359f49f20fa91faf49e1ac70a159615cb03
Afzal <afzalahmeddc@gmail.com>
2025-12-09 16:55:38 +0530

    changes in controllers/auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -79,8 +79,8 @@ const authController = {
       const token = jwt.sign(temp, process.env.JWT_SECRET, { expiresIn: "2h" });
       res.cookie("token", token, {
         httpOnly: true,
-        secure: false,
-        sameSite: "none",
+        secure: true,
+        sameSite: "None",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
 

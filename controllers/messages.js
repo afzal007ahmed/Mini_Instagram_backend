73e337425ac407da7de56f01ba677364c99d1078
Afzal <afzalahmeddc@gmail.com>
2025-12-15 17:23:28 +0530

    change in message router

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,43 +1,62 @@
 const { getIO, getUserSocketIdMap } = require("../socket");
-
+const { Op } = require("sequelize");
 const { messages, users } = require("../models/index");
 const jwt = require("jsonwebtoken");
 
- const messagesController = {
+const messagesController = {
   add: async (req, res) => {
     try {
       const { id } = jwt.verify(req.cookies.token, process.env.JWT_SECRET);
-      const io = getIO() ;
-      const userSocketMap = getUserSocketIdMap() ;
+      const io = getIO();
+      const userSocketMap = getUserSocketIdMap();
       await messages.create({
         from_id: id,
         to_id: req.params.id,
         message: req.body.message,
       });
       const user = await users.findOne({
-        where : {
-            id : id
-        }
-      })
+        where: {
+          id: id,
+        },
+      });
 
-      io.to(userSocketMap[ req.params.id ]).emit("new-message" , { 
-        message : req.body.message ,
-        name : user.name ,
-        id : id 
-       })
+      io.to(userSocketMap[req.params.id]).emit("new-message", {
+        message: req.body.message,
+        name: user.name,
+        id: id,
+      });
 
-       res.send({
-         success : true ,
-         error : false 
-       })
-
+      res.send({
+        success: true,
+        error: false,
+      });
     } catch (error) {
-        res.status(500).send({
-            success : false ,
-            error : error.message || "Something went wrong."
-        })
+      res.status(500).send({
+        success: false,
+        error: error.message || "Something went wrong.",
+      });
     }
+  },
+  getAll: async (req, res) => {
+    try {
+      const { id } = jwt.verify(req.cookies.token, process.env.JWT_SECRET);
+      await messages.findAll({
+        where: {
+          [Op.or]: [
+            {
+              from_id: id,
+              to_id: req.params.id,
+            },
+            {
+              to_id: id,
+              from_id: req.params.id,
+            },
+          ],
+        },
+        order : [["createdAt" , "Asc"]]
+      });
+    } catch (error) {}
   },
 };
 
-module.exports = { messagesController } ;
+module.exports = { messagesController };
--- routes/message.js
+++ routes/message.js
@@ -3,5 +3,6 @@ const { messagesController } = require('../controllers/messages');
 const messageRouter = require('express').Router() ;
 
 messageRouter.post('/:id/add' , messagesController.add ) ;
+messageRouter.get('/:id/all' , messagesController.getAll ) ;
 
 module.exports= { messageRouter } ;

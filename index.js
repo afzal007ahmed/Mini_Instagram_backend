95b0eb9f8b607a7e88d4735bb4fbfd45b85d9b1b
Afzal <afzalahmeddc@gmail.com>
2025-12-15 15:47:57 +0530

    change in logic for user status

--- index.js
+++ index.js
@@ -1,48 +1,64 @@
 require("dotenv").config();
 const { sequelize } = require("./config/db.config");
-require("./models/index");
+const { status } = require("./models/index");
 const { app } = require("./app");
 const http = require("http");
 const httpServer = http.createServer(app);
 const { socketIOSetup } = require("./socket");
-const { status } = require("./models/index");
 const { io, userSocketIdMap } = socketIOSetup(httpServer);
 
 io.on("connection", (socket) => {
-  socket.on("register", async({ userId }) => {
+  socket.on("register", async ({ userId }) => {
     try {
       socket.user_id = userId;
       userSocketIdMap[userId] = socket.id;
       const onlineStatus = await status.findOne({
-        where : {
-          user_id : userId 
-        }
+        where: {
+          user_id: userId,
+        },
       });
-      if( onlineStatus ) {
-        await status.update({
-          status : true 
-        } , { where : {
-          user_id : userId 
-        }})
-      }
-      else{
+      if (onlineStatus) {
+        await status.update(
+          {
+            status: true,
+          },
+          {
+            where: {
+              user_id: userId,
+            },
+          }
+        );
+      } else {
         await status.create({
-          user_id : userId ,
-          status : true 
-        })
+          user_id: userId,
+          status: true,
+        });
       }
       socket.emit("register-response", {
         message: "Connection established.",
       });
-
     } catch (error) {
       socket.emit("register-response", {
         message: error.message || "Something went wrong.",
       });
     }
   });
-  socket.on("disconnect", () => {
-    delete userSocketIdMap[socket.user_id];
+  socket.on("disconnect", async () => {
+    try {
+      await status.update({
+        status : false
+      } , {
+        where : {
+          user_id : socket.user_id 
+        }
+      })
+      delete userSocketIdMap[socket.user_id];
+      io.emit("disconnect-response" , {});
+    } catch (error) { 
+      socket.emit("disconnect-response" , {
+        message : error.message || "Something went wrong."
+      }) 
+    }
   });
 });
 
